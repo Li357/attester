ef9739ceb580797bd3db98c80200e1d2052c15b7
Andrew Li <andrewli357@gmail.com>
2022-02-01 23:43:41 -0500

    Remove identifying info

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-rsync -av -progress -e "ssh -i ~/.ssh/andrewli.pem" /Users/andrewli/Desktop/attester admin@3.86.39.56:/home/admin
+rsync -av -progress -e "ssh -i ~/.ssh/andrewli.pem" /Users/andrewli/Desktop/attester admin@184.73.48.229:/home/admin
